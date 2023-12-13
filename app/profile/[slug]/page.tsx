2e328e261a1925e0ef20feb1cbb2c3cfc6b2977a
Invincible Inventor <invincibleinventor@gmail.com>
2023-12-13 17:01:00 +0530

    Ok

--- app/profile/[slug]/page.tsx
+++ app/profile/[slug]/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import PostComponent from "@/components/PostComponent"
 import { createClient } from "@/utils/supabase/client"
 import { useEffect, useState } from "react"
 export default function Page({ params }: { params: { slug: string } }) {
@@ -11,6 +12,14 @@ const [loading,setLoading]  = useState(true)
     const [imfollowing,setImFollowing] = useState(false)
     const [followers,setFollowers] = useState(0)
     const [found,setFound] = useState(true)
+    const [posts,setPosts] = useState([])
+    useEffect(()=>{
+        async function get(){
+            const {data,error} = await supabase.from('posts').select('*').eq('handle',params.slug)
+            setPosts(data)
+        }
+        get()
+    },[])
     useEffect(()=>{
         async function get(){
             const {data:{session}} = await supabase.auth.getSession()
@@ -20,7 +29,8 @@ const [loading,setLoading]  = useState(true)
                 console.log(pe)
                 setFound(false)
             }
-            else{
+            else{if(pd){
+ {               console.log(pd)
                 setName(pd[0].name)
                 setAbout(pd[0].about)
                 setImage(pd[0].image)
@@ -38,21 +48,27 @@ const [loading,setLoading]  = useState(true)
                     }
                 }
             }
-    }
+              }  }}
     get()
     },[imfollowing])
     return !loading ? (<div className="flex flex-col">
         <div className="relative h-64">
-        <div className="w-full h-48 bg-blue-400"></div>
-        <img className="absolute w-24 h-24 rounded-lg bottom-5 md:left-12 left-6" src={image}></img>
-        <button className={`absolute rounded-full text-xs font-bold bottom-9 md:right-12 right-6 px-8 py-4 ${!imfollowing?'bg-red-400 text-white border-2 border-red-400':'bg-white text-red-500 border-2 border-red-400'}`}>{imfollowing?'Unfollow':'Follow'}</button>
+        <div className="h-48 m-4 bg-red-200 rounded-lg shadow-lg w-[calc(100%)-32px]"></div>
+        <img className="absolute w-24 h-24 rounded-lg shadow-lg bottom-2 md:left-12 left-6" src={image}></img>
+        <button className={`absolute rounded-full text-xs font-bold bottom-7 md:right-12 right-6 px-8 py-3 shadow-lg ${!imfollowing?'bg-red-400 text-white border-2 border-red-400':'bg-white text-red-500 border-2 border-red-400'}`}>{imfollowing?'Unfollow':'Follow'}</button>
         </div>
-        <div className="flex flex-col gap-2 ml-6 md:ml-12">
+        <div className="flex flex-col gap-2 mt-2 ml-8 md:ml-14">
         <div className="flex flex-row items-center content-center gap-2 ">
             <h1 className="text-xl font-semibold">{name}</h1>
-            <h1 className="text-sm font-normal text-neutral-400">@{params.slug}</h1>
+            <h1 className="text-xs font-normal text-neutral-400">@{params.slug}</h1>
         </div>
-        <h1 className="text-sm font-normal text-neutral-400">{about}</h1>
+        <h1 className="pr-12 text-sm font-normal leading-relaxed text-neutral-400">{about}</h1>
 </div>
+<h1 className="my-4 mb-1 ml-8 text-xl font-semibold">Posts</h1>
+<div className='flex flex-col gap-2 animate-in hiddenscroll'>
+    
+    {!loading ? ( posts.map((post) => (
+  <PostComponent id={post.id} key={post.id} image={post.image} dp={post.dp} handle={post.handle} name={post.name} description={post.content}/>
+   ))):<h1>Loading...</h1>} </div> 
     </div>) : (<>Loading...</>)
 }
