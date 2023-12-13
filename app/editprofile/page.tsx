06a9af7c8a7f5574573fbc5dae0342fa88b91f2e
That Typical Arch Fanboi <101926371+invincibleinventor@users.noreply.github.com>
2023-12-13 22:05:22 +0530

    Update page.tsx

--- app/editprofile/page.tsx
+++ app/editprofile/page.tsx
@@ -57,7 +57,7 @@ export default function Create(){
   
        
      <form
-       className="flex flex-col justify-center w-full gap-2 my-auto ml-auto animate-in text-foreground"
+       className="flex flex-col justify-center w-full gap-2 my-auto ml-auto pr-6 animate-in text-foreground"
        action={create}>
        
        <label className="text-md" htmlFor="name">
