ae881f839bcccd8e6c22d892794b0e946fccedb3
Invincible Inventor <invincibleinventor@gmail.com>
2023-12-13 15:07:51 +0530

    Ok

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -35,7 +35,7 @@ console.log(error)
        </label>
        <textarea
         onChange={(e:any)=>setContent(e.target.value)}
-         className="px-4 py-2 mb-6 border rounded-md h-2/6 bg-inherit"
+         className="w-full px-4 py-2 mb-6 mr-4 border rounded-md h-2/6 bg-inherit"
          name="content"
          placeholder="Please Type Out Your Concern (30 - 100 Characters)"
          required
