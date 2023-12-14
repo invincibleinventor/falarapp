703b6bca4100f89110830b49ead6639720c062b6
That Typical Arch Fanboi <101926371+invincibleinventor@users.noreply.github.com>
2023-12-14 13:17:15 +0530

    Update PostComponent.jsx

--- components/PostComponent.jsx
+++ components/PostComponent.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link"
 export default function Post(props){
    return(
-      <Link href={`/posts/${props.id}`}>
+      <Link href={`/post/${props.id}`}>
         <div className="w-full">
             <div className="flex flex-row gap-4 px-8 py-3">
                 <Link href={`/profile/${props.handle}`} className="flex flex-col shrink-0">
