10895a046880841f4425cea0f76079f34f32d71c
Invincible Inventor <invincibleinventor@gmail.com>
2023-12-19 17:59:12 +0530

    ok

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -3,11 +3,18 @@ import { createClient } from "@/utils/supabase/client";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { Oval } from "react-loader-spinner";
+import Markdown from "react-markdown";
 export default function App({ params }: { params: { slug: string } }) {
     const supabase = createClient()
-    const [post,setPost] = useState({})
+    const [post,setPost] = useState("")
+    const [author,setAuthor] = useState("")
+    const [content,setContent] = useState("")
+    const [title,setTitle] = useState("")
+    const [name,setName] = useState("")
+    const [cover,setCover] = useState("")
+    const [profile,setProfile] = useState("")
     const [error,setError] = useState(false)
-    const [loading,setLoading] = useState(false)
+    const [loading,setLoading] = useState(true)
 
 
     useEffect(()=>{
@@ -16,8 +23,15 @@ export default function App({ params }: { params: { slug: string } }) {
         if(data && data.length>0){
             setError(false)
             setPost(data[0])
-            setLoading(false)
+            const {data:u} = await supabase.from('user').select('*').eq('id',data[0]["poster"])
+            setAuthor(u[0]["handle"])
+            setName(u[0].name)
+            setProfile(u[0].image)
+            setContent(data[0]["content"])
+            setTitle(data[0]["title"])
+            setCover(data[0]["cover"])
             
+            setLoading(false)
         }
         else if (error || data.length==0){
             setError(true)
@@ -41,8 +55,15 @@ set()    }
         }
         {!error &&
         <div>
-            <div className="w-full h-48">
+            <div className="relative w-full h-64"  >
+                <img src={cover?cover:'https://picsum.photos/2000/3000'} className="absolute top-0 bottom-0 left-0 right-0 object-cover w-full h-64"></img>
+                <div className="flex flex-col items-center content-center w-full h-64 px-10 bg-black bg-opacity-50 backdrop-blur-md">
+                   <div className="flex flex-col items-center content-center gap-4 my-auto"> <h1 className="mx-auto text-2xl font-semibold leading-9 text-center text-gray-200">{title}</h1>
+                   <Link href={'/profile/'+author} className="flex flex-row items-center content-center gap-2 px-4 py-2 bg-white rounded-lg bg-opacity-90 w-max"><img className="w-5 h-5 rounded-lg" src={profile}></img><h1 className="text-xs">{name}</h1></Link>
+                </div>
+                </div>
             </div>
+            <Markdown className="px-5 py-5">{content}</Markdown>
             </div>
         }
             </div>
