0655abdfea4e5da2508cb540dc4271ed84351d1f
That Typical Arch Fanboi <101926371+invincibleinventor@users.noreply.github.com>
2023-12-21 23:02:35 +0530

    Update HeaderItem.tsx

--- components/HeaderItem.tsx
+++ components/HeaderItem.tsx
@@ -28,7 +28,7 @@ export default function App (props:pros) {
     return(
         <Link href={props.link} className={`flex flex-row rounded-full px-3 md:px-[14px] md:pr-8 w-max md:w-full py-[10px] mx-2 md:mx-0 md:space-x-[16px] rounded-lg transition-all ease-linear duration-100 cursor-pointer items-center content-center  ${a.opened?'':''}`}>
   <svg className={`w-[30px] stroke-2 p-[2px] text-black h-[30px] ${a.opened?'text-red-500':'text-black'}`} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">{props.url}</svg>
-    <span className={`text-black text-lg md:inline-block hidden font-poppins ${a.opened?'font-medium text-red-400':'font-normal'} pt-[0px]`}>{props.name}</span>
+    <span className={`text-black md:inline-block hidden font-poppins ${a.opened?'font-medium text-red-400':'font-normal'} pt-[0px]`}>{props.name}</span>
   </Link>
     )
 }
