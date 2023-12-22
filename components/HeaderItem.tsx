16a54543869d126b37761f46545d380d1b099618
BalaTBR <101926371+invincibleinventor@users.noreply.github.com>
2023-12-22 19:04:34 +0530

    Update HeaderItem.tsx

--- components/HeaderItem.tsx
+++ components/HeaderItem.tsx
@@ -26,7 +26,7 @@ export default function App (props:pros) {
     
 
     return(
-        <Link href={props.link} className={`flex flex-row rounded-lg px-3 md:px-[6px] md:pr-8 w-max md:w-full py-[10px] mx-2 md:mx-0 md:space-x-[16px] rounded-lg transition-all ease-linear duration-100 cursor-pointer items-center content-center  ${a.opened?'':''}`}>
+        <Link href={props.link} className={`flex flex-row rounded-lg px-3 md:px-[6px] md:pr-8 w-max md:w-full py-[10px] md:py-2 mx-2 md:mx-0 md:space-x-[16px] rounded-lg transition-all ease-linear duration-100 cursor-pointer items-center content-center  ${a.opened?'':''}`}>
   <svg className={`w-[26px] stroke-2 p-[2px] text-black h-[26px] ${a.opened?'text-black':'text-gray-700'}`} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">{props.url}</svg>
     <span className={`text-gray-700 text-sm md:inline-block hidden font-poppins ${a.opened?'font-medium text-black':'font-normal'} pt-[0px]`}>{props.name}</span>
   </Link>
