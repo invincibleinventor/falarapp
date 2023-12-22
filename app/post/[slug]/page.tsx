e261e8dc223bcdd8fe6d78b443c6abaace96ffb4
BalaTBR <101926371+invincibleinventor@users.noreply.github.com>
2023-12-22 19:22:57 +0530

    Update page.tsx

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -64,7 +64,7 @@ const components = {
             <div className="flex flex-col gap-2 mx-auto max-w-max">
             <h1 className="mx-auto text-lg font-semibold text-center text-black">That Content Doesn't Exist</h1>
             <h1 className="mx-auto text-sm text-center text-neutral-400">That page does not exist. It must have been moved or deleted. Please refresh if you think that is not the case</h1>
-            <Link href="/" className={`w-max mx-auto rounded-lg text-xs font-bold mt-3 px-8 py-3 ${(1==1)?'bg-black text-white':'bg-white  border-2 '}`}>Return Back</Link>
+            <Link href="/" className={`w-max mx-auto text-xs font-bold mt-3 px-8 py-3 ${(1==1)?'bg-black text-white':'bg-white  border-2 '}`}>Return Back</Link>
       
             </div>
             
@@ -80,7 +80,7 @@ const components = {
               <h1 className="text-4xl font-extrabold md:leading-[calc(15*4px)] md:text-5xl">{title}</h1>
               <div className="flex flex-row items-center content-center justify-between mt-6 text-md">
                 <Link  href={"/profile/"+author} className="flex flex-row items-center content-center">
-                <img className="mr-3 rounded-md w-7 h-7" src={profile}></img>
+                <img className="mr-3  w-7 h-7" src={profile}></img>
                 <h1 className="font-semibold">{name}</h1>
                 </Link>
                 <h1 className="text-sm font-normal">Posted {timeAgo.format(Date.now() - time)}</h1>
