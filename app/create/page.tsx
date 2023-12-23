6922f8fc4da5208cc41b27fcf32cb82f2439c057
BalaTBR <101926371+invincibleinventor@users.noreply.github.com>
2023-12-23 22:55:42 +0530

    Update page.tsx

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -66,13 +66,14 @@ console.log(error)
         }
         else{
           if(changed){
-            alert('rched')
+            
             const {data,error} = await supabase.from('posts').select('*').eq('poster',user.id).order('id',{ascending:false}).limit(1)
-    if(data){alert(data[0]["id"])}
+    
             let newCover = await coverChange(data[0]["id"])
-            alert(newCover)
-            const {error:es} = await supabase.from('posts').update({'cover':newCover}).eq('id',data[0]["id"])
-if(es){alert(es)}else{redirect('/')}
+          
+            const {error:es} = await supabase.from('posts').upsert({'cover':newCover,'id':data[0]["id"]})
+if(es){alert(es)}
+            redirect('/')
           }
         }
         
