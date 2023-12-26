c504e7a6e4cbc1f7322948f06bfbe2b4c1e11587
BalaTBR <101926371+invincibleinventor@users.noreply.github.com>
2023-12-26 17:00:03 +0530

    Update page.tsx

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -162,7 +162,7 @@ console.log('above')
        
     <svg   xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m5 16l-1 4l4-1L19.586 7.414a2 2 0 0 0 0-2.828l-.172-.172a2 2 0 0 0-2.828 0zM15 6l3 3m-5 11h8"/></svg>
    
-        <h1 className="hidden text-sm md:inline-block">Edit Post</h1>
+        <h1 className="text-sm inline-block">Edit Post</h1>
     </Link>
 }
               <div className="flex flex-row items-center content-center justify-between mt-6 text-md">
