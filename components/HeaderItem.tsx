c39f8bd531c03fc4429f1211e974d16982db5afd
BalaTBR <101926371+invincibleinventor@users.noreply.github.com>
2024-03-24 10:07:23 +0530

    Update HeaderItem.tsx

--- components/HeaderItem.tsx
+++ components/HeaderItem.tsx
@@ -20,7 +20,7 @@ export default function App(props:any) {
   return (
     <Link
       href={props.link}
-      className={`mx-2 flex w-max cursor-pointer flex-row content-center items-center  px-3 py-[10px] transition-all bg-gray-50 md:bg-transparent border border-gray-200 h-[calc(13*4px)] md:h-max md:border-none md:rounded-none  duration-100 ease-linear md:mx-0 md:w-full md:space-x-[10px] md:px-[6px] md:py-[8px] md:pr-8  ${
+      className={`mx-2 flex w-max cursor-pointer flex-row content-center items-center  px-3 py-[10px] transition-all bg-gray-50 md:bg-transparent border border-black h-[calc(13*4px)] md:h-max md:border-none md:rounded-none  duration-100 ease-linear md:mx-0 md:w-full md:space-x-[10px] md:px-[6px] md:py-[8px] md:pr-8  ${
         a.opened ? "" : ""
       }`}
     >
