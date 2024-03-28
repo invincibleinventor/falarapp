c269448d1323846ddd10bc865afd404cc5321e0f
BalaTBR <101926371+invincibleinventor@users.noreply.github.com>
2024-03-28 09:28:00 +0530

    Update PostComponent.tsx

--- components/PostComponent.tsx
+++ components/PostComponent.tsx
@@ -38,7 +38,7 @@ export default function Post(props:any) {
           <div className="flex h-max flex-col gap-[8px] px-6 py-6 ">
             <div className="flex flex-row items-center content-center gap-2 h-max shrink-0">
              <Link className="w-10 h-10" href={`/profile/`+props.handle}>
-              <Image width={30} height={30} className="w-10 h-10 rounded-sm shrink-0" src={props.dp} alt="dp" />
+              <Image width={30} height={30} className="w-10 h-10 shrink-0" src={props.dp} alt="dp" />
              </Link>
               <Link href={`/profile/`+props.handle} className="flex flex-row gap-[2px]">
                 <div className="flex flex-col content-center flex-1 gap-[2px] py-0 pl-2 rounded-lg">
@@ -53,7 +53,7 @@ export default function Post(props:any) {
 
               </Link>
               
-              <div className="pl-2 ml-auto">
+              <div className="pl-3 ml-auto">
                 <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><g fill="none"><path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"/><path fill="currentColor" d="M12 16.5a1.5 1.5 0 1 1 0 3a1.5 1.5 0 0 1 0-3m0-6a1.5 1.5 0 1 1 0 3a1.5 1.5 0 0 1 0-3m0-6a1.5 1.5 0 1 1 0 3a1.5 1.5 0 0 1 0-3"/></g></svg>
               </div>
             </div>
