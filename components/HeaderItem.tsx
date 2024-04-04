d113acd2b0471b5a43dc77823350bdbc76b09851
BalaTBR <101926371+invincibleinventor@users.noreply.github.com>
2024-04-04 11:29:21 +0530

    Update HeaderItem.tsx

--- components/HeaderItem.tsx
+++ components/HeaderItem.tsx
@@ -39,7 +39,7 @@ export default function App(props:any) {
       </svg>
       <span
         className={`font-inter font-medium hidden text-sm md:text-base text-black md:inline-block ${
-          a.opened ? "font-medium text-gray-200" : "font-medium text-gray-200"
+          a.opened ? "font-medium text-white" : "font-medium text-gray-200"
         } pt-[0px]`}
       >
         {props.name}
