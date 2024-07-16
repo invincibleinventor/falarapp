0ccce8cddb4418558c5ba1ca5aa7a0ed637f22c7
BalaTBR <101926371+invincibleinventor@users.noreply.github.com>
2024-07-16 22:54:20 +0530

    Update SettingsBar.tsx

--- components/SettingsBar.tsx
+++ components/SettingsBar.tsx
@@ -14,7 +14,7 @@ url={
   */
   const loc = usePathname();
   return (
-    <div className="mb-auto ">
+    <div className="mb-4 md:mb-auto ">
       <div className={`absolute md:static left-0  flex w-max  flex-col items-start px-2 md:p-0 ${loc.startsWith("/post/") ? "md:flex" : ""}`}>
        
         <div className="flex flex-row md:flex-col space-x-1 md:space-x-0 py-4 pt-[14px] md:mt-[calc(17*4px)] md:space-y-[0px] md:px-4 md:pt-[4px]">
