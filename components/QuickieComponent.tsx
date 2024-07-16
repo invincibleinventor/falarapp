c7d6ee22239edbfdb4b6b5e314a03de550bbdc2e
BalaTBR <101926371+invincibleinventor@users.noreply.github.com>
2024-07-16 23:39:27 +0530

    Update QuickieComponent.tsx

--- components/QuickieComponent.tsx
+++ components/QuickieComponent.tsx
@@ -6,6 +6,7 @@ import { redirect } from "next/navigation";
 import BookMarksComponent from "./QuickieBookMarksComponent";
 import LikeComponent from "./QuickieLikeComponent";
 export default function Post(props:any) {
+  const timeStamp = new Date().getTime()
   let x = props.name;
   if (x.length >= 11) {
     x = x.slice(0, 7);
@@ -80,7 +81,7 @@ photocount = props.image.length;
               {photocount>0  &&
               <div className={photocount==1?"w-full border ml-0 rounded-md mt-4 aspect-video h-full mb-4":"mt-4 mb-4 md:gap-2 gap-1 grid-cols-2 grid"}>
                 {props.image.map((image:string) => 
-                  <img onClick={()=>window.open(image,'_blank')?.focus()} className="object-cover w-full border rounded-md h-max aspect-video" src={image}></img>
+                  <img onClick={()=>window.open(image,'_blank')?.focus()} className="object-cover w-full border rounded-md h-max aspect-video" src={image+"?"+timeStamp}></img>
                 )}
               </div>
 }
