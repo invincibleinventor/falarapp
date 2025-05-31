04a0e5925bf3c3bdc3162ad47bb4dc729729224b
Invincible Inventor <invincibleinventor@gmail.com>
2025-05-31 19:54:22 +0530

    ok

--- app/bookmarks/layout.tsx
+++ app/bookmarks/layout.tsx
@@ -8,7 +8,7 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
   const pathname = usePathname();
 
   return (
-    <div className="flex-1 h-screen p-0 py-2 overflow-hidden">
+    <div className="overflow-hidden flex-1 p-0 py-2 h-screen">
             <div className="p-4 py-2 pb-2 mx-1 md:mx-1">
 
       {pathname=='/bookmarks' &&
@@ -20,7 +20,7 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
 
       }
      </div>
-      <div className="flex flex-row flex-grow w-auto text-sm font-medium text-white font-inter">
+      <div className="flex flex-row flex-grow w-auto text-base font-medium text-white font-pops">
         <Link href="/bookmarks" className={`w-1/2 py-4 text-center ${pathname == "/bookmarks" ? "border-b-2 border-b-primary-800" : ""}`}>
           Posts
         </Link>
