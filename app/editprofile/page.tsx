c98095aef418740f99083a9cbbb1e830de6a9d55
Invincible Inventor <invincibleinventor@gmail.com>
2025-06-11 16:28:57 +0530

    ok

--- app/editprofile/page.tsx
+++ app/editprofile/page.tsx
@@ -20,16 +20,25 @@ export default async function CreatePage() {
   const image = user.user_metadata.avatar_url;
 
   return (
-    <div className="flex flex-col justify-center items-center px-10 mx-auto w-full max-w-lg h-screen">
+    <div className="flex flex-col justify-center items-center mx-auto w-screen h-screen">
+           <div   className="flex overflow-hidden relative flex-col gap-2 justify-center mx-5 my-auto w-full max-w-lg">
+           <div className="absolute right-[20%] -top-[20%] w-[400px] h-[400px] rounded-full bg-gradient-to-br  shadow-neutral-700 from-neutral-800 to-neutral-600 shadow-xl"></div>
+
+           <div className="absolute left-[50%] bottom-[30%] w-96 h-96 rounded-full bg-gradient-to-br from-neutral-900 to-neutral-800  shadow-xl"></div>
+           <div className="absolute left-[25%] bottom-[0%] w-96 h-96 rounded-full bg-gradient-to-b from-neutral-600 to-neutral-400  shadow-xl"></div>
+           <div className="absolute left-[5%] -bottom-[10%] w-96 h-96 rounded-full bg-gradient-to-l from-neutral-600 to-neutral-900  shadow-xl"></div>
+      
+      
+      <div className="absolute right-[40%] top-[5%] w-[400px] h-[400px] rounded-full bg-gradient-to-br from-neutral-800 to-neutral-700 shadow-xl"></div>
       <form
-        className="flex flex-col gap-2 justify-center my-auto ml-auto w-full animate-in text-foreground"
+        className="flex flex-col gap-2 justify-center px-10 py-16  my-auto w-full max-w-lg rounded-3xl filter border-[0.1px] shadow-xl backdrop-blur-2xl border-neutral-800 bg-black/10 animate-in text-foreground"
         action={createUser}
       >
         <input type="hidden" name="email" value={email} />
         <input type="hidden" name="image" value={image} />
 
         <h1 className="text-2xl font-semibold text-white">Welcome To {AppConfig.title}</h1>
-        <h1 className="pb-10 text-base font-normal text-neutral-400">
+        <h1 className="pb-10 text-base font-normal text-white">
           Let us setup your profile on {AppConfig.title}. Setup your profile to continue.
         </h1>
 
@@ -37,7 +46,7 @@ export default async function CreatePage() {
           Name
         </label>
         <input
-          className="px-4 py-2 mb-6 text-sm rounded-md border outline-none bg-black/20 text-neutral-300 border-neutral-900 focus:outline-primary-800"
+          className="px-4 py-2 mb-6 text-sm rounded-md outline-none bg-black/20 text-neutral-300 focus:outline-primary-800"
           name="name"
           placeholder="Please Type Out Your Name"
           required
@@ -49,7 +58,7 @@ export default async function CreatePage() {
           Username - You cannot change this later
         </label>
         <input
-          className="px-4 py-2 mb-6 text-sm rounded-md border outline-none bg-black/20 text-neutral-300 border-neutral-900 focus:outline-primary-800"
+          className="px-4 py-2 mb-6 text-sm rounded-md outline-none bg-black/20 text-neutral-300 focus:outline-primary-800"
           name="handle"
           placeholder="Please Type Out Your Username"
           required
@@ -61,7 +70,7 @@ export default async function CreatePage() {
           About
         </label>
         <textarea
-          className="px-4 py-2 mb-6 text-sm rounded-md border outline-none bg-black/20 text-neutral-300 border-neutral-900 focus:outline-primary-800"
+          className="px-4 py-2 mb-6 text-sm rounded-md outline-none bg-black/20 text-neutral-300 focus:outline-primary-800"
           name="about"
           placeholder="Please Type About Yourself"
           required
@@ -69,10 +78,11 @@ export default async function CreatePage() {
           maxLength={100}
         />
 
-        <button className="px-8 py-4 mx-auto mb-2 w-max text-sm font-medium text-white rounded-full bg-primary-700">
+        <button className="px-8 py-4 mx-auto mb-2 w-max text-sm font-medium text-white rounded-full shadow-xl bg-primary-700">
           Setup Your Account
         </button>
       </form>
+      </div>
     </div>
   );
 }
